8494125b2cb12c64e9779517590b4b2dd65ccff5
Benedict Uttley <benedictuttley@hotmail.com>
2019-02-22 19:21:28 +0000

    Added cublas multiplication to C expm().

--- expm_matlab.m
+++ expm_matlab.m
@@ -1,4 +1,4 @@
- dlmwrite('/home/c1673666/expm_Cuda/cuda/Quantum-Simulator/read.txt', double(rand(1000,1000)), 'delimiter', ' ')
+ dlmwrite('/home/c1673666/expm_Cuda/cuda/Quantum-Simulator/read.txt', double(rand(10,10)), 'delimiter', ' ')
 
 M = dlmread('read.txt'); 
 tic; C = expm(M); toc;
