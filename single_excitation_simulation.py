6a05fede5e516b756ef63953933e6f1e61648878
benedictuttley <benedictuttley@hotmail.com>
2019-05-01 19:41:38 +0100

    ready for labs.

--- single_excitation_simulation.py
+++ single_excitation_simulation.py
@@ -1,4 +1,24 @@
-# Single excitation case
+# Single Excitation Simulator
+# ---------------------------
+
+# Models the information transfer in the network with one bit of information being transferred.
+
+# File Input:
+# -----------
+
+# [1] network_SES.txt encoding the topology of the network
+# [2] PSI(0)_SES.txt encoding the initial system state
+
+# Arguments:
+# -----------
+
+# [1] Network size (number of spins in the network)
+# [2] Initial time to simulate
+# [3] Final time to simulate
+# [4] Time step increment
+
+# Reference: Langbein, F.C., Schirmer, S. and Jonckheere, E., 2015, December. Time optimal information transfer in spintronics networks.
+
 
 import sys
 import os
@@ -14,6 +34,11 @@ import matplotlib.gridspec as gridspec
 
 # Get the number of spin particles in the network
 Nspin = abs(int(sys.argv[1]))
+# Get time step information
+timeInterval_begin = abs(int(sys.argv[2]))
+timeInterval_end = abs(int(sys.argv[3]))
+increment = abs(int(sys.argv[4]))
+
 list_of_spins = []
 for spin in range(1, Nspin+1):
 	list_of_spins.append("Spin: " + str(spin))
@@ -21,6 +46,7 @@ for spin in range(1, Nspin+1):
 # Construct the hamiltonian
 H = np.zeros((Nspin, Nspin))*1j
 Conn = np.zeros((Nspin, Nspin))
+
 # Get the connectivity matrix describing the network topology:
 with open('network_SES.txt', 'r') as f:
 	i = 0
@@ -39,11 +65,11 @@ with open('PSI(0)_SES.txt', 'r') as f:
 
 
 # Simulate system for each time period
-print("Single excitation state prepared at Spin 1: ")
-time_steps = [0.0, 0.5, 1.0, 1.5, 2.0, 2.5]
+time_steps = []
 fidelities = []
 info = [[] for x in range(Nspin)] 
-for T in time_steps:
+for T in range(timeInterval_begin, timeInterval_end, increment):
+	time_steps.append(T)
 	# Calculate the unitary operator U(t):
 	U = sp.linalg.expm(-1j*T*H) 
 	# Find the new state at time t:
@@ -69,7 +95,7 @@ for T in time_steps:
 # Visualize transfer fidelity change over time steps
 print("Vizualising...")
 x = time_steps
-fig = plt.figure(figsize=(10, 10), dpi=80)
+fig = plt.figure(figsize=(20, 15), dpi=80)
 fig.suptitle("Probability of Information Transfer From Spin 1 To Connected Spins")
 fig.canvas.set_window_title('Simulation Vizualisation')
 
@@ -78,16 +104,16 @@ axs = []
 
 i = 0
 for spin in list_of_spins:
-	y = info[i] # Fetch corresponding probability array for spin
+	y = info[i]
 	axs.append(fig.add_subplot(gs[i], title="*** Fidelity For Spin: 1 -> " + spin + " ***",
-    xticks=time_steps, ylim=(-0.05, 1.05), yticks=[0.0, 0.25, 0.50, 0.75, 1.0], xlabel="Time(s)",
+		ylim=(-0.05, 1.05), yticks=[0.0, 0.25, 0.50, 0.75, 1.0], xlabel="Time(s)", 
     ylabel="Transfer Fidelity"))
-    axs[-1].spines['right'].set_visible(False)
-    axs[-1].spines['top'].set_visible(False)
-    axs[-1].set_facecolor("#d3d3d3")
-    axs[-1].set_autoscaley_on(False)
-    axs[-1].plot(x,y,'go-', linewidth=2)
-    i+=1
+	axs[-1].spines['right'].set_visible(False)
+	axs[-1].spines['top'].set_visible(False)
+	axs[-1].set_facecolor("#d3d3d3")
+	axs[-1].set_autoscaley_on(False)
+	axs[-1].plot(x,y,'gx-', linewidth=2)
+	i+=1
 
 plt.subplots_adjust(wspace=0.25, hspace=0.25)
 plt.show()
